74112d6c12af4f6a6fff2284c8f9a78bac733719
HeshamMoawad <heshammowad120120@yahoo.com>
2023-10-26 08:36:24 +0200

    add constants file

---
+++ core/_constants.py
@@ -0,0 +1,24 @@
+
+
+DB_PATH = "Data\database.db"
+
+SOURCES = "Sources"
+AGENTS = "Agents"
+PROJECTS = "Projects"
+DATA  = "Data"
+LIVE_DATA = "Live_Data"
+LEADS = "Leads"
+
+
+TABLES = [
+    SOURCES ,
+    AGENTS ,
+    PROJECTS ,
+    DATA ,
+    LIVE_DATA ,
+    LEADS ,
+]
+
+
+
+
