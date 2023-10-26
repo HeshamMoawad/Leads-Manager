b6292b461304168210bf457d4960d3fcc0adf686
HeshamMoawad <heshammowad120120@yahoo.com>
2023-10-26 12:34:11 +0200

    create table model

--- QPandasManager/core/models.py
+++ QPandasManager/core/models.py
@@ -1,17 +1,33 @@
 from pandas import DataFrame , read_sql_query
 from PyQt5.QtCore import QObject
-import sqlite3
+from utils import (
+    replace_value_with_id ,
+    merge_lists_to_dict ,
+)
+import sqlite3 , typing
 
 
 
-class Table(DataFrame):
+class Table(DataFrame,QObject):
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self,table_name:str,con:sqlite3.Connection,) -> None:
+        super(Table,self).__init__()
+        self.table_name = table_name
+        self.con = con
+        self.__dict__ = read_sql_query(f"SELECT * FROM {table_name}",con).__dict__
+
+
+    def get_original(self,relations:typing.Dict[str,dict]={})-> DataFrame :
+        data = self.copy()
+        for column in self.columns :
+            if column.endswith("_id") and column.replace("_id","") in relations.keys() :
+                data[column] = data[column].map(relations[column.replace("_id","")])
+        print(data)
+
+    def get_relations(self)-> dict:
+        return merge_lists_to_dict(self[self.columns[0]].to_list(),self[self.columns[1]].to_list())
 
     
-
-
 
 
 class DataReader(QObject):
@@ -83,4 +99,16 @@ class DataReader(QObject):
             """ ,
             con = self.connection
         )
+
+from _constants import LIVE_DATA , AGENTS , SOURCES , PROJECTS , DATA
+
+connection = sqlite3.connect("Data\database.db")
+
+tt = Table(SOURCES,connection)
+t2 = Table(DATA,connection)
+
+t2.get_original({
+    SOURCES.lower() : tt.get_relations()
+})
+# print(tt)
 
